fd9ee5b6fe9103bad75142944b3f5e16d849e7bc
phw-nightingale <937855602@qq.com>
2017-11-20 17:33:27 +0800

    完善框架

---
+++ src/main/java/cn/it/phw/ms/common/BaseEntity.java
@@ -0,0 +1,15 @@
+package cn.it.phw.ms.common;
+
+import java.io.Serializable;
+
+/**
+ *
+ */
+public class BaseEntity implements Serializable {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
+}
