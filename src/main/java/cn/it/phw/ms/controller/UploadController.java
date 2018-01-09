bfca43cce51994eff65e60f1dcc393706894c0f7
phw <937855602@qq.com>
2018-01-09 14:08:47 +0800

    Authority Building...

--- src/main/java/cn/it/phw/ms/controller/UploadController.java
+++ src/main/java/cn/it/phw/ms/controller/UploadController.java
@@ -5,12 +5,9 @@ import cn.it.phw.ms.service.UploadService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.io.File;
 
 @Controller
 public class UploadController extends BaseController {
--- src/main/java/cn/it/phw/ms/service/BaseService.java
+++ src/main/java/cn/it/phw/ms/service/BaseService.java
@@ -9,6 +9,6 @@ package cn.it.phw.ms.service;
  * 包括了一些基础增删改查的操作
  *
  */
-public interface BaseService {
+public interface BaseService<T> {
 
 }
